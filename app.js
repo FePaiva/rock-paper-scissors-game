e25f79917eb5cb3839e8b15d6a2666d86969d392
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-12-12 10:50:20 -0800

    player is able to make a selection to start game.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_SELECTION = 'PAPER';
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid selectio! We have selected ${DEFAULT_SELECTION} for you!`);
+    return DEFAULT_SELECTION;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting ...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
