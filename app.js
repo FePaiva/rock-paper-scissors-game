612b746a9382816d87ed47fb3ba7b2d648b886ab
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-12-12 13:20:18 -0800

    implemented core game logic

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_SELECTION = 'PAPER';
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WIN = 'PLAYER_WINS';
+const RESULT_PLAYER_LOST = 'PLAYER_LOST';
 
 let gameIsRunning = false;
 
@@ -13,10 +16,36 @@ const getPlayerChoice = function () {
     ''
   ).toUpperCase();
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-    alert(`Invalid selectio! We have selected ${DEFAULT_SELECTION} for you!`);
+    alert(`Invalid selection! We have selected ${DEFAULT_SELECTION} for you!`);
     return DEFAULT_SELECTION;
   }
   return selection;
+};
+
+const getComputerChoice = function () {
+  // let computerOptions = [ROCK, PAPER, SCISSORS];
+  const computerChoice = Math.random();
+  if (computerChoice < 0.34) {
+    return ROCK;
+  } else if (computerChoice < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const determineWinner = function (compChoice, playChoice) {
+  if (compChoice === playChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (compChoice === ROCK && playChoice === PAPER) ||
+    (compChoice === PAPER && playChoice === SCISSORS) ||
+    (compChoice === SCISSORS && playChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WIN;
+  } else {
+    return RESULT_PLAYER_LOST;
+  }
 };
 
 startGameBtn.addEventListener('click', function () {
@@ -26,5 +55,9 @@ startGameBtn.addEventListener('click', function () {
   gameIsRunning = true;
   console.log('Game is starting ...');
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  console.log('player', playerSelection);
+  const computerSelection = getComputerChoice();
+  console.log('computer', computerSelection);
+  const winner = determineWinner(computerSelection, playerSelection);
+  console.log('result', winner);
 });
